8bedab01d28dd8bab158822eeafd27d6d86313af
mody abdo <mohamadody123456@gmail.com>
2022-10-06 17:57:11 +0200

    finished currency names-keys

--- lib/views/currency_names.dart
+++ lib/views/currency_names.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import '../core/utils/currencies_service.dart';
 
 class CurrencyReference extends StatefulWidget {
-  const CurrencyReference({Key? key,}) : super(key: key);
+  const CurrencyReference({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<CurrencyReference> createState() => _CurrencyReferenceState();
@@ -22,20 +24,41 @@ class _CurrencyReferenceState extends State<CurrencyReference> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-       body: FutureBuilder<Map>(
-    future: allCurrencies,
-    builder: (context,currenciesSnapshot) {
-      if (currenciesSnapshot.connectionState == ConnectionState.waiting) {
-        return const Center(child: CircularProgressIndicator());
-      }
-      return ListView.builder(
-        itemBuilder: (context, index) {
-          return  Text('${currenciesSnapshot.data?.keys}');
-        },
-      );
-    }
-    ),
-    ),
+        appBar: AppBar(),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: FutureBuilder<Map>(
+              future: allCurrencies,
+              builder: (context, currenciesSnapshot) {
+                if (currenciesSnapshot.connectionState ==
+                    ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                return ListView.builder(
+                  itemCount: currenciesSnapshot.data?.length,
+                  itemBuilder: (context, index) {
+                    final currencyShort =
+                        currenciesSnapshot.data?.keys.toList()[index];
+                    final currencyName =
+                        currenciesSnapshot.data?.values.toList()[index];
+
+                    return Card(
+                      elevation: 5,
+                      child: ListTile(
+                        leading: Text(
+                          '$currencyShort:',
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        title: Text('$currencyName',
+                            style: const TextStyle(fontSize: 20)),
+                      ),
+                    );
+                  },
+                );
+              }),
+        ),
+      ),
     );
   }
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -11,9 +11,9 @@ class HomePage extends StatefulWidget {
 
   @override
   State<HomePage> createState() => _HomePageState();
-  }
+}
 
-  class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> {
   late Future<RatesModel> rates;
   late Future<Map> allCurrencies;
   final formKey = GlobalKey<FormState>();
@@ -26,36 +26,41 @@ class HomePage extends StatefulWidget {
     });
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
-                      elevation: 2,
-                      //backgroundColor: Colors.white,
-                    ),
-        drawer:  Drawer(
+          elevation: 2,
+          //backgroundColor: Colors.white,
+        ),
+        drawer: Drawer(
           child: ListView(
-           children: [
-             FutureBuilder<Map>(
-                 future: allCurrencies,
-                 builder: (context,currenciesSnapshot) {
-                   if (currenciesSnapshot.connectionState == ConnectionState.waiting) {
-                     return const Center(child: CircularProgressIndicator());
-                   }
-                   return ListTile(
-                       leading: const Icon(Icons.add_chart_outlined),
-                       title: const Text('Currency Reference'),
-                       onTap: (){
-                         Navigator.of(context).push(
-                             MaterialPageRoute(builder: (context) =>
-                                 const CurrencyReference()));
-                       }
-                   );
-                 }
-             ),
-           ],
+            children: [
+              FutureBuilder<Map>(
+                  future: allCurrencies,
+                  builder: (context, currenciesSnapshot) {
+                    if (currenciesSnapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    return Padding(
+                      padding: const EdgeInsets.only(top: 15),
+                      child: Card(
+                        elevation: 4,
+                        child: ListTile(
+                            leading: const Icon(Icons.add_chart_outlined),
+                            title: const Text('Currency Reference'),
+                            onTap: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => const CurrencyReference()));
+                            }),
+                      ),
+                    );
+                  }),
+            ],
           ),
         ),
         body: SizedBox(
@@ -63,46 +68,48 @@ class HomePage extends StatefulWidget {
           width: MediaQuery.of(context).size.width,
           child: FutureBuilder<RatesModel>(
             future: rates,
-            builder: (context,ratesSnapshot){
-              if(ratesSnapshot.connectionState == ConnectionState.waiting) {
+            builder: (context, ratesSnapshot) {
+              if (ratesSnapshot.connectionState == ConnectionState.waiting) {
                 return const Center(child: CircularProgressIndicator());
               }
               return Center(
                 child: FutureBuilder<Map>(
                   future: allCurrencies,
-                  builder: (context,currenciesSnapshot){
-                  if(currenciesSnapshot.connectionState == ConnectionState.waiting)
-                {
-                return const Center(child: CircularProgressIndicator());
-                }
-                return SingleChildScrollView(
-                child: Form(
-                key: formKey,
-                child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                const SizedBox(height: 30,),
-                UsdToAny(
-                rates: ratesSnapshot.data!.rates,
-                currencies: currenciesSnapshot.data!
+                  builder: (context, currenciesSnapshot) {
+                    if (currenciesSnapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    return SingleChildScrollView(
+                      child: Form(
+                        key: formKey,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            UsdToAny(
+                                rates: ratesSnapshot.data!.rates,
+                                currencies: currenciesSnapshot.data!),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            AnyToAny(
+                              rates: ratesSnapshot.data!.rates,
+                              currencies: currenciesSnapshot.data!,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
                 ),
-
-                const SizedBox(height: 20,),
-                AnyToAny(
-                rates: ratesSnapshot.data!.rates,
-                currencies: currenciesSnapshot.data!,
-                ),
-                ],
-                ),
-                ),
-                );
-                },
-                ),
-                );
-                },
-                ),
-                ),
-                ),
-                );
-                }
-              }
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
